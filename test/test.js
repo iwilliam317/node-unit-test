6083c63f2acfb87de7e636534f75e8abd07de302
william <willshinji@gmail.com>
2019-01-11 12:15:13 -0200

    before - after hooks

--- test/test.js
+++ test/test.js
@@ -1,10 +1,21 @@
 const assert = require('assert')
 
 describe('String', () => {
+
+    before(() => {
+        console.log('##### BEFORE #####')
+    })
+
+    after(() => {
+        console.log('##### AFTER #####')
+    })
+    
     context('toUpperCase()', () => {
         it('should return capital letter', () => {
             const sentence = 'Hello Word'
             assert.equal(sentence.toUpperCase(), 'HELLO WORD')
         })
+
+        it('should be pending')
     })
 })
